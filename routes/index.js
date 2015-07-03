90666604f72fc7489dcfaf576596f4c2ab8ac1a0
gmatute <gmatute@ual.es>
2015-07-03 18:32:36 +0200

    Merge con rama creditos
    Merge con rama creditos

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.get('/author', quizController.author);
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
