2343a30ad841e9d5452bbfdf22e9eafe5b72173f
gmatute <gmatute@ual.es>
2015-08-01 23:14:59 +0200

    Lista de preguntas
    Tema 7.4 : Lista de preguntas

--- models/models.js
+++ models/models.js
@@ -39,9 +39,10 @@ sequelize.sync().then(function() {
   Quiz.count().then(function (count){
     if (count=== 0) {  //la tabla se iniciliza solo si está vacía
       Quiz.create({ pregunta: 'Capital de Italia',
-                    respuesta: 'Roma'
-                  })
-     .then(function(){console.log('Base de datos inicializada')});
+                      respuesta: 'Roma'})
+      Quiz.create({ pregunta: 'Capital de Portugal',
+                    respuesta: 'Lisboa'})
+      .then(function(){console.log('Base de datos inicializada')});
     }
   })
 });
